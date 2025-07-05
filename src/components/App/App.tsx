17121a1e55ce26769bb399a30bc9d0742ddcc087
KateChmil <chmilkatecat@gmail.com>
2025-07-05 17:25:15 +0300

    updated service

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,7 +2,7 @@ import css from './App.module.css'
 import { useState } from 'react';
 import toast, { Toaster } from 'react-hot-toast';
 
-import { fetchMovies } from '../../services/MovieService';
+import { fetchMovies } from '../../services/movieService';
 import type { Movie } from '../../types/movie';
 
 import SearchBar from '../SearchBar/SearchBar';
